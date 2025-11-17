68d1d7a83929b5d3a606cbb3d03b38fa01221a47
rond88 <r0nald19202006@gmail.com>
2025-11-17 11:50:40 +0100

    feat: add Bootstrap and Bootstrap Icons, implement blog component and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/shared/home/home';
+import { PlistBlogComponent } from './components/feature/blog/plist/plist';
 
 export const routes: Routes = [
-    {path: '', component:HomeComponent}
+    {path: '', component:HomeComponent},
+    {path: 'blog', component: PlistBlogComponent}
 ];
---
+++ src/app/components/feature/blog/plist/plist.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plist',
+  imports: [],
+  templateUrl: './plist.html',
+  styleUrl: './plist.css',
+})
+export class PlistBlogComponent {
+  
+}
